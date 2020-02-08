7db4dbc7492721e924df35aabc006a7cb0def0b9
Akash Munagala <akash.munagala@gmail.com>
2020-02-07 19:01:14 -0800

    Add Shaders

--- Model-Viewer/Application.cpp
+++ Model-Viewer/Application.cpp
@@ -1,13 +1,22 @@
 #include "stdafx.h"
+#include "Shader.h"
 #include "Window.h"
 
 int main() {
 	Window window;
+	
+	try {
+		Shader shader("solidColor");
+	} catch(const std::exception& e) {
+		std::cout << e.what() << std::endl;
+		return -1;
+	}
 
 	while(window.isOpen()) {
 		window.swapBuffers();
 		glfwPollEvents();
 	}
+
 
 	return 0;
 }
--- Model-Viewer/stdafx.h
+++ Model-Viewer/stdafx.h
@@ -1,3 +1,6 @@
 #pragma once
 
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
