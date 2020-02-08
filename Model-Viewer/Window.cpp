bef2f987ea225e27bd659086419147885a8088ea
Akash Munagala <akash.munagala@gmail.com>
2020-02-07 20:34:37 -0800

    Add OpenGL Debug Output

--- Model-Viewer/Window.cpp
+++ Model-Viewer/Window.cpp
@@ -3,8 +3,8 @@
 
 Window::Window() {
 	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	window = glfwCreateWindow(800, 600, "Model Viewer", nullptr, nullptr);
@@ -17,6 +17,19 @@ Window::Window() {
 	if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
 		throw std::runtime_error("Failed to initialize GLAD");
 	}
+
+#ifdef _DEBUG
+	glEnable(GL_DEBUG_OUTPUT);
+	glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+	glDebugMessageCallback(glDebugCallback, nullptr);
+	GLuint ignoredIDs[] = { 0x20071 };
+	glDebugMessageControl(GL_DEBUG_SOURCE_API, GL_DEBUG_TYPE_OTHER, GL_DONT_CARE, 1, ignoredIDs, GL_FALSE);
+	glfwSetErrorCallback([](int errorCode, const char* desc) {
+		char error[255];
+		sprintf_s(error, "GLFW Error %d: %s\n", errorCode, desc);
+		throw std::runtime_error(error);
+	});
+#endif
 	
 	glViewport(0, 0, 800, 600);
 	glfwSetFramebufferSizeCallback(window, [](GLFWwindow* window, int width, int height) {
